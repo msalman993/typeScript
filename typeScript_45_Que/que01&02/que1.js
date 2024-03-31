9f790ed79706b68b908be10a0bc81671d85b5b33
msalman993 <165561802+msalman993@users.noreply.github.com>
2024-03-31 19:47:00 +0500

    que01_&_que2

---
+++ typeScript_45_Que/que01&02/que1.js
@@ -0,0 +1,7 @@
+//Que # 1. Install [Node.js](https://nodejs.org/en/), [TypeScript](https://www.typescriptlang.org/download)  and [VS Code](https://code.visualstudio.com/) on your computer.
+//Que No 01 Completed.. Enviroment Prepaired For Coading typeScript.
+//Que # 2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+const person_Name = "Muhammad Salman";
+let message = `"Hello ${person_Name}, would you like to learn some typeScript todey"`;
+console.log(message);
+export {};
