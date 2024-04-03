71cec400b021c7b11a93120ad4dde6d56960a7d4
msalman993 <165561802+msalman993@users.noreply.github.com>
2024-04-03 21:15:06 +0500

    que07&08

---
+++ typeScript_45_Que/que07&08/que7.ts
@@ -0,0 +1,17 @@
+// Que 7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results. 
+
+// Que 8. You should create four lines that look like this:
+//8
+//8
+//8
+//8
+
+let addition = 5+3;
+let subtraction = 20-12;
+let multiplication = 2*4;
+let division = 24/3;
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
