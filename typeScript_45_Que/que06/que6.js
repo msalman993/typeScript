e6127f1f9d3c80bad2342344e0852273bcdf3a24
msalman993 <165561802+msalman993@users.noreply.github.com>
2024-04-03 21:08:07 +0500

    que06

---
+++ typeScript_45_Que/que06/que6.js
@@ -0,0 +1,6 @@
+"use strict";
+// Que # 6. Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+let nameWithWhiteSpaces = "\n\tsalman\t\n";
+console.log(nameWithWhiteSpaces);
+let nameWithoutWhiteSpaces = nameWithWhiteSpaces.trim();
+console.log(nameWithoutWhiteSpaces);
