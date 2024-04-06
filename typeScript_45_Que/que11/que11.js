bc5f7aa39373771dc6b11f195f31b38ff5db307a
msalman993 <165561802+msalman993@users.noreply.github.com>
2024-04-07 03:03:49 +0500

    Update que11.js

--- typeScript_45_Que/que11/que11.js
+++ typeScript_45_Que/que11/que11.js
@@ -1,6 +1,18 @@
-//Que # 11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
 var names = ["Shaqib", "Sami", "Asad", "Hasnain"];
 console.log(names[0]);
 console.log(names[1]);
 console.log(names[2]);
 console.log(names[3]);
+console.log("\n");
+//it can also be coade through for-loop as;
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+;
+console.log("\n");
+// It can also be coade thruogh for-of loop as;
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var eachName = names_1[_i];
+    console.log(eachName);
+}
+;
