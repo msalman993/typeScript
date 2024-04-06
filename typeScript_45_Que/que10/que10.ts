8701263b1cfa414b7d0d837c1dfdd917a553ea19
msalman993 <165561802+msalman993@users.noreply.github.com>
2024-04-06 16:06:43 +0500

    upload_que10

---
+++ typeScript_45_Que/que10/que10.ts
@@ -0,0 +1,14 @@
+//Que # 10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// Addition of two numbers. (tuesday 25, March) Coded by Muhammad Salman;
+let firstNum = 5;
+let secNum = 4;
+let Addition = firstNum + secNum;
+console.log(Addition)
+
+// multiplication of two numbers.(tuesday 25, March) Coded by Muhammad Salman;
+
+let firstNumb = 5;
+let secNumb = 4;
+let multiplication = firstNumb * secNumb;
+console.log(multiplication)
