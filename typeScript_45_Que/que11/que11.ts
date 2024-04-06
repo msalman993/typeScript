77e7fc7b6ce3b95041180d3e75353b2aa8b3a2d0
msalman993 <165561802+msalman993@users.noreply.github.com>
2024-04-07 02:57:58 +0500

    Update que11.ts

--- typeScript_45_Que/que11/que11.ts
+++ typeScript_45_Que/que11/que11.ts
@@ -5,3 +5,13 @@ console.log(names[0]);
 console.log(names[1]);
 console.log(names[2]);
 console.log(names[3]);
+
+//it can also be coade through for-loop as;
+for(let i = 0; i<names.length; i++){
+  console.log(names[i])
+};
+// It can also be coade thruogh for-of loop as;
+for(let eachName of names){
+  console.log(eachName)
+};
+
