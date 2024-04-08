a948c7127c1400f05938d95d54b05a42c7d3f1aa
msalman993 <165561802+msalman993@users.noreply.github.com>
2024-04-09 04:34:55 +0500

    Add files via upload

---
+++ typeScript_45_Que/que12/que12.ts
@@ -0,0 +1,6 @@
+//12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names: string[] = ["Muhammad Shaqib", "Sami Ullah", "Asad Kurram", "Muhammad Hasnain"];
+for (let name of names) {
+    console.log(`I will feel happy if you start programming Dear ${name}`)
+}
