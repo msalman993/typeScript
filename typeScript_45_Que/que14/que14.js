d8777779e073182f8eca8b3b593cf6e4aef5b38e
msalman993 <165561802+msalman993@users.noreply.github.com>
2024-04-12 12:35:52 +0500

    upload_que14

---
+++ typeScript_45_Que/que14/que14.js
@@ -0,0 +1,6 @@
+// 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+let guestList = ["shahqib", "sami", "asad"];
+for (let val of guestList) {
+    console.log(`Dear "${val}", I invite you for a dinner for upcomming night. Thank You.`);
+}
+export {};
