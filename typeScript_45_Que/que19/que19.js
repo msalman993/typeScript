818a7f112e52b65afb5bff221dd771c77c50613f
msalman993 <coadsa993@gmail.com>
2024-04-29 21:04:42 +0500

    upload_que19

---
+++ typeScript_45_Que/que19/que19.js
@@ -0,0 +1,2 @@
+var guestList = ["shahqib", "sami", "asad"];
+console.log("I am inviting ".concat(guestList.length, " people to dinner."));
---
+++ typeScript_45_Que/que19/que19.ts
@@ -0,0 +1,2 @@
+let guestList: string[] = ["shahqib", "sami", "asad"];
+console.log(`I am inviting ${guestList.length} people to dinner.`);
