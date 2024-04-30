de58aaa05f0ece2aa9f5ef05e3c0c7b333b29b3c
msalman993 <coadsa993@gmail.com>
2024-04-30 20:13:23 +0500

    upload_que25

---
+++ typeScript_45_Que/que25/que25.ts
@@ -0,0 +1,15 @@
+// Que 25
+
+let alien_color = "green";
+
+// Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+if (alien_color === "green") {
+    console.log("Congratulation Yuo have earned 5 points")
+};
+
+// Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+
+if (alien_color === "blue") {
+    console.log("Congratulation Yuo have earned 5 points")
+};
