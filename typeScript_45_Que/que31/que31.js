f7db2983d3961c7e60b7af1527276abd8c2dc466
msalman993 <coadsa993@gmail.com>
2024-05-01 15:25:47 +0500

    upload_que31

---
+++ typeScript_45_Que/que31/que31.js
@@ -0,0 +1,8 @@
+// Que 31
+//Add an if test to make sure the list of users is empty.
+//• If the list is empty, print the message We need to find some users!
+let userNames = [];
+if (userNames.length === 0) {
+    console.log("We need to find some users!");
+}
+export {};
