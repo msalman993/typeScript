acddc62c51b3421adf9432aa00fc15c22d886e66
msalman993 <coadsa993@gmail.com>
2024-05-01 18:34:35 +0500

    upload_que36

---
+++ typeScript_45_Que/que36/que36.js
@@ -0,0 +1,8 @@
+// Que 36
+//Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+function make_shirt(size, text) {
+    console.log(`${size} is the size of my shirt\nWrite "${text}" on my shirt`);
+}
+;
+make_shirt(5, "Salman");
+export {};
