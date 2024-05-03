2d92a1279860a1b9bdb76ae82cefa5f22002b873
msalman993 <coadsa993@gmail.com>
2024-05-03 06:22:25 +0500

    upload_que21

---
+++ typeScript_45_Que/que21/que21.ts
@@ -0,0 +1,11 @@
+// Que 21
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let student = {
+    studentName: "salman",
+    class: 6,
+    isPass: true,
+    age: 22
+};
+console.log(student); //it print all properties
+console.log(student.class) // it print only class
