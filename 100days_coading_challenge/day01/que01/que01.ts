906cd711ae048a71954443a8a21ade11be0798ba
msalman993 <165561802+msalman993@users.noreply.github.com>
2024-05-08 19:52:03 +0500

    Create que01

---
+++ 100days_coading_challenge/day01/que01/que01.ts
@@ -0,0 +1,2 @@
+//Que # 1. Install [Node.js](https://nodejs.org/en/), [TypeScript](https://www.typescriptlang.org/download)  and [VS Code](https://code.visualstudio.com/) on your computer.
+//Que No 01 Completed.. Enviroment Prepaired For Coading typeScript.
