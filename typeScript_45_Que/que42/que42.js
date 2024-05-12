7acdf3ddec7a9e9294337fba1f1241f76e09fff6
msalman993 <coadsa993@gmail.com>
2024-05-12 23:05:41 +0500

    update_que42

--- typeScript_45_Que/que42/que42.js
+++ typeScript_45_Que/que42/que42.js
@@ -10,23 +10,26 @@ function make_album(artist, title, tracks) {
     }
     return album;
 }
-function show_albums(albums) {
-    for (const album of albums) {
-        console.log(album);
-    }
-}
-function make_great(albums) {
-    for (let i = 0; i < albums.length; i++) {
-        albums[i].artist = 'the Great ' + albums[i].artist;
-    }
-}
 const albums = [
     make_album("Atif Aslam", "21"),
     make_album("Atif Aslam", "rafta rafta sanam.."),
     make_album("Rahat Fatih Ali Khan", "Tmhe dil lagi bhool jane paregi", 16)
 ];
+function make_great(albums) {
+    for (let i = 0; i < albums.length; i++) {
+        albums[i].artist = 'the Great ' + albums[i].artist;
+    }
+}
+function show_albums(albums) {
+    for (const album of albums) {
+        console.log(album);
+    }
+}
 show_albums(albums);
+// here I call show_albums() to show my albums without adding the phrase 'The Great' by passing albums(array created above) as an argument.
 make_great(albums);
+// here I call make_great() to change each artist's name  by passing albums(array created above) as an argument.
 show_albums(albums);
 export {};
-// I have solved this que question with the help of chatgpt.. Now I am trying to understand this logic with Youtube and chatgpt.
+// here I call show_albums() to show my albums having updated artist's names.
+// I am happy to express that this time I have solved this question with my clear concept about all logics used above.
