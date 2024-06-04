8bad33441f5b207a6cf884fafb8d5e1049ad0590
msalman993 <coadsa993@gmail.com>
2024-06-04 06:46:56 +0500

    que09

---
+++ 100days_coading_challenge/day03/que09/que9.js
@@ -0,0 +1,5 @@
+// Que # 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+const favNumber = 993;
+let message = `I am going to tell you about my favourite number which is "${favNumber}".`;
+console.log(message);
+export {};
